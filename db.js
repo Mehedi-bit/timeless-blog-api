43ecc775d990957d8ece346e69d04f5dffd3df3a
Md. Mehedi Hasan Emon <60240261+Mehedi-bit@users.noreply.github.com>
2023-01-01 23:19:41 +0600

    database moved from local

--- db.js
+++ db.js
@@ -1,9 +1,16 @@
 import mysql2 from 'mysql2';
 
+// export const db = mysql2.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'inkorrect',
+//     database: 'blog'
+// })
+
 export const db = mysql2.createConnection({
-    host: 'localhost',
+    host: 'containers-us-west-174.railway.app',
     user: 'root',
-    password: 'inkorrect',
-    database: 'blog'
+    password: 'wNMyNMqdiXD1B1iDzz9x',
+    database: 'railway'
 })
 
