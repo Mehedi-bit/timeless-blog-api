5a51fa4e7f1277e32bd2508aa652f7f86eb8b2ee
Md. Mehedi Hasan Emon <60240261+Mehedi-bit@users.noreply.github.com>
2023-01-02 01:32:18 +0600

    deployment test

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ import express from 'express';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 import postRoutes from './routes/posts.js';
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import multer from 'multer';
 
-// dotenv.config();
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
 
 
 const storage = multer.diskStorage({
